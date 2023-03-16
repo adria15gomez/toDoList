6ab219f124a59193631f8b0275463d67e31b62c7
adria15gomez <adria15gomez@gmail.com>
2023-03-16 21:42:56 +0100

    first commit

---
+++ prueba/index.php
@@ -0,0 +1,25 @@
+<?php
+$nombre = "Adria";
+
+define("NUMERO_PI", 3.14);
+const GRAVEDAD = 9.8;
+
+
+//echo NUMERO_PI;
+
+//echo GRAVEDAD;
+
+/*$coder = "Adria";
+echo $coder;
+echo "\n";
+
+print_r($coder);
+echo "\n";
+
+var_dump($coder);*/
+
+
+$patatas_en_costal = "10 patatas en un costal";
+$patatas_en_mano = 5;
+
+echo $patatas_en_costal + $patatas_en_mano;
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GridComponent } from './grid/grid.component';
+import { ButtonComponent } from './button/button.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GridComponent,
+    ButtonComponent,
+ 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
